bea9d2216241c1e41add88d65643b67445bb597f
ukinori <lendariog@gmail.com>
2018-04-18 10:54:09 -0300

    working 100%

--- src/br/com/cryptocurrency/SpiderTest.java
+++ src/br/com/cryptocurrency/SpiderTest.java
@@ -11,6 +11,6 @@ public class SpiderTest
     public static void main(String[] args)
     {
         Spider spider = new Spider();
-        spider.search("https://google.com.br", "");
+        spider.search("https://www.google.com", "");
     }
 }
